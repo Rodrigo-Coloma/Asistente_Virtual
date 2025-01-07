f0e44148b847d9f1fb91ef43457eb83ef79ab707
Rodrigo-Coloma <rjcolgut@gmail.com>
2025-01-07 17:04:38 +0100

    factos working

--- main.py
+++ main.py
@@ -35,11 +35,13 @@ def main():
 
     # --- user authentification --- 
     if "username" not in st.session_state:
-        with st.form('credentials'):
-            username = st.text_input('Username: ',placeholder='your_username')
-            password = st.text_input('Password: ',placeholder='your_password',type='password')
-            if st.form_submit_button('Login',type='primary'):
-                mgt.user_login(username,password)
+        log_cols = (1,2,1)
+        with log_cols[1]:
+            with st.form('credentials'):
+                username = st.text_input('Username: ',placeholder='your_username')
+                password = st.text_input('Password: ',placeholder='your_password',type='password')
+                if st.form_submit_button('Login',type='primary'):
+                    mgt.user_login(username,password)
     else:
         st.session_state.tools = ['Chat', 'Email', 'Code']
         st.session_state.tool = st.sidebar.selectbox('Herramienta', st.session_state.tools,0)
