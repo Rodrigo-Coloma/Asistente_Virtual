07da1b0a5ede396a57c40117c0e83f041ea377e6
Rodrigo-Coloma <rjcolgut@gmail.com>
2025-01-08 08:44:54 +0100

    rag working

--- utils/chat.py
+++ utils/chat.py
@@ -187,7 +187,7 @@ def chat():
             messages = [HumanMessage(content=m["content"]) if m["role"] == "user" else AIMessage(content=m["content"]) for m in st.session_state.messages]
             
             if st.session_state.factos:
-                st.markdown(get_factos(llm_factos, messages))
+                st.markdown(get_factos(llm_factos, messages,prompt))
             elif not st.session_state.use_rag:
                 st.write_stream(stream_llm_response(llm_stream, messages))
             else:
