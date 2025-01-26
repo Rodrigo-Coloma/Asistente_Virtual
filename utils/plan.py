cb5257e799b7b5e7a0268110473fe6e5fc68a57c
Rodrigo Coloma <rjcolgut@gmail.com>
2025-01-26 14:23:41 +0100

    little fixes

--- utils/plan.py
+++ utils/plan.py
@@ -27,8 +27,7 @@ def gpt_connect():
 def get_plan_context(vector_db, llm):
     retriever = vector_db.as_retriever()
     prompt = ChatPromptTemplate.from_messages([
-        MessagesPlaceholder(variable_name="messages"),
-        ("user", "Retrieve all the available information about the Plantilla Plan de acción{input}"),
+        ("user", "Retrieve all the available information about the Plantilla Plan de acción"),
     ])
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
 
@@ -39,10 +38,9 @@ def get_plan_response(llm):
 
     prompt = ChatPromptTemplate.from_messages([
         ("system",
-        """Eres un asistente de redacción de planes de acción para el departamento de datos de una empresa hotelera. Ayudame a redactar un plan de acción en base a la siguiente plantilla: 
-        \n {context}\n
-        El plan resultante debe tener unicamente los cuatro apartados del original: necesidad, objetivos, Acciones por parte de Data & Analytics y Medición del éxito"""),
-        ("user", "{input}"),
+        """Eres un asistente de redacción de planes de acción para el departamento de datos de una empresa hotelera. Ayudame a redactar un plan de acción utilizando la estructura y formato de la siguiente plantilla: 
+        \n {context}\n El contenido del plan de acción generado debe estar basado en las siguientes notas generadas durante una reunión con el departamento correspondiente {input}
+        El plan resultante debe tener unicamente cuatro apartados: necesidad, objetivos, Acciones por parte de Data & Analytics y Medición del éxito""")
     ])
     stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
 
@@ -67,14 +65,14 @@ def plan():
     llm_stream = ChatOpenAI(model="gpt-4o", temperature=0.4)
     cols = st.columns(2)
     with cols[0]:
-        notes = st.text_area("Copia aqui las notas ara el plan de acción", height=280)
+        notes = st.text_area("Copia aqui las notas para el plan de acción", height=280)
 
         instructions = st.text_area("Instrucciones addicionales", height=140)
 
         instructions = f"Instrucciones adicionales: {instructions}"
 
         if st.button("Crear el Plan de Acción"):
-            input = f"Estas son las notas tomadas en la reunión en las que te tienes que basar para elaborar el plan de acción\n{notes}\n{instructions}"
+            input = f"{notes}\n{instructions}"
 
             with cols[1]:
                 st.write_stream(stream_llm_plan_response(llm_stream, input))
