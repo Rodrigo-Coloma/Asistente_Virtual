a8b5e7c69830c0759f343df6e3132e447b1c9821
Rodrigo-Coloma <rjcolgut@gmail.com>
2025-01-30 17:44:56 +0100

    img v0

--- utils/img.py
+++ utils/img.py
@@ -8,15 +8,20 @@ import os
 def img():
     openai.api_key = st.secrets['GPTAPIKEY']
     prompt=st.text_input("Describe aquí la imagen que deseas generar")
+    resolution = st.sidebar.selectbox('Resolución',["256x256","512x512","1024x1024"])
+    n_img = st.slider("Número de imás")
 
     if st.button("Generar imagen",type= "primary"):
-        response = openai.images.generate(
-            prompt = prompt,
-            model = "dall-e-3"
-        )
-        img_url = response.data[0].url
-        req_response = requests.get(url=img_url, timeout=20)
-        path = "./data/images/temp.png"
+        with st.spinner("generando la imagen..."):
+            response = openai.images.generate(
+                prompt = prompt,
+                model = "dall-e-3",
+                size = resolution,
+                n = n_img
+            )
+            img_url = response.data[0].url
+            req_response = requests.get(url=img_url, timeout=20)
+            path = "./data/images/temp.png"
         if req_response.status_code == 200:
             with open(path, "wb") as output:
                 output.write(req_response.content)
